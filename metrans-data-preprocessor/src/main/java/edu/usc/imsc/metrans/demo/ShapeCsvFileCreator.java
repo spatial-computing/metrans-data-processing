5f1b6dbfc8d3890cb306e8a2fe0f7413b9e005fe
duykienvp <duykienvp@yahoo.com.vn>
2018-03-01 23:05:40 -0800

    recreate shape.csv file

--- metrans-data-preprocessor/src/main/java/edu/usc/imsc/metrans/demo/ShapeCsvFileCreator.java
+++ metrans-data-preprocessor/src/main/java/edu/usc/imsc/metrans/demo/ShapeCsvFileCreator.java
@@ -1,12 +1,5 @@
 package edu.usc.imsc.metrans.demo;
 
-import com.opencsv.CSVWriter;
-import com.opencsv.bean.ColumnPositionMappingStrategy;
-import com.opencsv.bean.HeaderColumnNameMappingStrategy;
-import com.opencsv.bean.StatefulBeanToCsv;
-import com.opencsv.bean.StatefulBeanToCsvBuilder;
-import com.opencsv.exceptions.CsvDataTypeMismatchException;
-import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import edu.usc.imsc.metrans.gtfsutil.GtfsStore;
 import edu.usc.imsc.metrans.gtfsutil.GtfsUtil;
 import edu.usc.imsc.metrans.gtfsutil.ShapeInfoItem;
@@ -16,10 +9,9 @@ import org.onebusaway.gtfs.model.Trip;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -54,6 +46,10 @@ public class ShapeCsvFileCreator {
             String tripId = routeMaxLengthTrip.get(routeId).getId().getId();
             ArrayList<StopTime> stopTimes = gtfsStore.getTripStopTimes().get(tripId);
 
+            if (routeId.startsWith("9")) {
+                logger.info(routeId + "--" + GtfsUtil.getShortRouteId(routeId) + "--" + tripId);
+            }
+
             for (StopTime stopTime : stopTimes) {
                 ShapeInfoItem item = new ShapeInfoItem();
 
@@ -73,37 +69,26 @@ public class ShapeCsvFileCreator {
 
     public static void saveShapeInfoItems(ArrayList<ShapeInfoItem> items) {
         try {
-            class CustomMappingStrategy<T> extends ColumnPositionMappingStrategy<T> {
-                private final String[] HEADER = new String[]{"route_id", "stop_sequence", "stop_name", "lat", "lon", "stop_id"};
+            String[] columns = new String[]{"route_id", "stop_sequence", "stop_name", "lat", "lon", "stop_id"};
+            PrintWriter writer = new PrintWriter(new FileWriter(SHAPE_CSV_FILE));
 
-                @Override
-                public String[] generateHeader() {
-                    return HEADER;
-                }
+            // write header
+            for (int i = 0; i < columns.length - 1; i++)
+                writer.print(columns[i] + ",");
+            writer.println(columns[columns.length-1]);
+
+            for (ShapeInfoItem item : items) {
+                writer.print(item.getRoute_id() + ", ");
+                writer.print(item.getStop_sequence() + ", ");
+                writer.print(item.getStop_name() + ", ");
+                writer.print(item.getLat() + ", ");
+                writer.print(item.getLon() + ", ");
+                writer.println(item.getStop_id());
             }
 
-            Writer writer = Files.newBufferedWriter(Paths.get(SHAPE_CSV_FILE));
-
-            ColumnPositionMappingStrategy mappingStrategy =
-                    new ColumnPositionMappingStrategy();
-
-            mappingStrategy.setType(ShapeInfoItem.class);
-            //Fields in ShapeInfoItem
-            String[] columns = new String[]{"route_id", "stop_sequence", "stop_name", "lat", "lon", "stop_id"};
-            //Setting the colums for mappingStrategy
-            mappingStrategy.setColumnMapping(columns);
-
-            CustomMappingStrategy customMappingStrategy = new CustomMappingStrategy();
-            customMappingStrategy.setType(ShapeInfoItem.class);
-
-            StatefulBeanToCsv beanToCsv = new StatefulBeanToCsvBuilder(writer)
-                    .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
-//                    .withMappingStrategy(mappingStrategy)
-                    .withMappingStrategy(customMappingStrategy)
-                    .build();
-
-            beanToCsv.write(items);
-        }  catch (CsvRequiredFieldEmptyException | IOException | CsvDataTypeMismatchException e) {
+            writer.flush();
+            writer.close();
+        }  catch (IOException e) {
             e.printStackTrace();
         }
     }
