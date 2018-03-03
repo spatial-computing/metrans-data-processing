2ef3ceeedf5d02a69234008ea57517ac4d6a37ee
duykienvp <duykienvp@yahoo.com.vn>
2018-03-03 00:23:03 -0800

    remove "year" from stats info

--- metransws/src/main/java/edu/usc/imsc/metrans/ws/stats/StatsInfo.java
+++ metransws/src/main/java/edu/usc/imsc/metrans/ws/stats/StatsInfo.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 import java.util.TreeMap;
 
 public class StatsInfo {
-    private TreeMap<String, Double> year = new TreeMap<>();
+//    private TreeMap<String, Double> year = new TreeMap<>();
     private ArrayList<Double> month = new ArrayList<>();
     private ArrayList<Double> week = new ArrayList<>();
     private ArrayList<Double> day = new ArrayList<>();
 
-    public TreeMap<String, Double> getYear() {
-        return year;
-    }
+//    public TreeMap<String, Double> getYear() {
+//        return year;
+//    }
 
-    public void setYear(TreeMap<String, Double> year) {
-        this.year = year;
-    }
+//    public void setYear(TreeMap<String, Double> year) {
+//        this.year = year;
+//    }
 
     public ArrayList<Double> getMonth() {
         return month;
